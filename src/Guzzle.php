9583e8c20278a4dec0589a3c7d2944153c534319
XAKEPEHOK <icq-475204@ya.ru>
2020-02-11 16:55:48 +0300

    init

---
+++ src/Guzzle.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created for plugin-component-guzzle
+ * Datetime: 11.02.2020 16:49
+ * @author Timur Kasumov aka XAKEPEHOK
+ */
+
+namespace Leadvertex\Plugin\Components\Guzzle;
+
+
+
+use GuzzleHttp\Client;
+
+class Guzzle
+{
+
+    /** @var Client */
+    private static $client;
+
+    private function __construct() {}
+
+    public static function getInstance(): Client
+    {
+        $selfUri = $_ENV['PLUGIN_SELF_URL'];
+        $selfType = $_ENV['PLUGIN_SELF_TYPE'];
+
+        if (self::$client === null) {
+            self::$client = new Client([
+                'headers' => [
+                    'User-Agent' => "LV-{$selfType}-PLUGIN/1.0 ({$selfUri})"
+                ],
+            ]);
+        }
+
+        return self::$client;
+    }
+
+}
